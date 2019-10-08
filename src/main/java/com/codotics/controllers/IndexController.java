479fdd0aadceedb93840ec6fc841fa2cd4baa131
chhaba <chhaba@gmail.com>
2019-10-08 09:27:58 +0530

    Initial commit.

---
+++ src/main/java/com/codotics/controllers/IndexController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.codotics.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author Codotics Technologies
+ *
+ */
+@Controller
+public class IndexController {
+
+	@RequestMapping({ "", "/", "/index" })
+	public String getIndexPage() {
+		return "index";
+	}
+}
